ba667bad0ac0517590f678fabf6d295cd5e2389e
kittychi <kitty.sham@gmail.com>
2015-12-14 01:30:50 -0800

    updating day13 to use defaultdict

--- day13.py
+++ day13.py
@@ -1,31 +1,46 @@
 import inputs, itertools
-def day13(in_):
+from collections import defaultdict
+def day13(in_, b = False):
 	nodes = set()
-	edges = {}
+	relation = defaultdict(dict)
 	for line in in_:
 		p1, _, d, val, _, _, _, _,_,_,p2 = line.split()
 		p2 = p2[:-1]
 		nodes.add(p1)
 		nodes.add(p2)
-		curval = edges.setdefault(p1, {}).setdefault(p2, 0)
-		edges[p1][p2] += int(val) if d == 'gain' else -int(val)
-		edges.setdefault(p2, {}).setdefault(p1, 0)
-		edges[p2][p1] = edges[p1][p2]
+		relation[p1].setdefault(p2, 0) 
+		relation[p1][p2] += int(val) if d == 'gain' else -int(val)
+		relation[p2][p1] = relation[p1][p2]
 
 	#part b
-	edges['me'] = {}
-	for p in nodes:
-		edges[p]['me']=0
-		edges['me'][p] = 0
-	nodes.add('me')
+	if b:
+		relation['me'] = {}
+		for p in nodes:
+			relation[p]['me']=0
+			relation['me'][p] = 0
+		nodes.add('me')
 
-	allcombos = list(itertools.permutations(nodes))
-	allhapdelta = []
+	allcombos = list(itertools.permutations(list(nodes)[:-1]))
+	removed = list(nodes)[-1]
+	allhapdelta = {}
 	for combo in allcombos:
 		first = list(combo)
+		first.append(removed)
 		next = [first[i%len(first)] for i in range(1, len(first)+1)]
 		pairs = list(zip(first, next))
-		hap_ = [edges[x][y] for x, y in pairs]
-		allhapdelta.append(sum(hap_))
-	print(max(allhapdelta))
+		hap_ = [relation[x][y] for x, y in pairs]
+		allhapdelta.setdefault(sum(hap_), []).append(first)
+
+	sort = sorted(allhapdelta.items(), reverse=True)
+	for h, li_ in sort[0:3]:
+		print(h)
+		for l in li_:
+			first = list(l)
+			neighbour = [first[i%len(first)] for i in range(1, len(first)+1)]
+			pairs = list(zip(first, neighbour))
+			hap_ = [relation[x][y] for x, y in pairs]
+			print( list(zip(first, hap_)))
+
 day13(inputs.day13)
+print('b')
+day13(inputs.day13, True)
